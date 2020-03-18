afd785d4b8fc0faacff2d0a5aaaaa1b0d491de57
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-03-17 20:59:06 -0700

    finishing urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include
 from django.contrib import admin
-# from django.urls import path
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('project.urls')),
-    # path('accounts/', include('accounts.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('project.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
